9bf5a0e52426d08d82da3e84dda3a2aa3904c48e
정 휘 <whistle84621@gmail.com>
2026-05-12 17:49:22 +0900

    week06 mission

--- week06/mission/app/src/main/java/com/example/umc_10th_android_paul_week04/di/NetworkModule.kt
+++ week06/mission/app/src/main/java/com/example/umc_10th_android_paul_week04/di/NetworkModule.kt
@@ -1,5 +1,13 @@
 package com.example.umc_10th_android_paul_week04.di
 
+import com.example.umc_10th_android_paul_week04.data.remote.ApiClient
+import com.example.umc_10th_android_paul_week04.data.remote.MyPageService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
